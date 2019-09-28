5fb778975d0dcba90e8948c01be5fd56deb1fd80
eduardohrmsnt <51173394+eduardohrmsnt@users.noreply.github.com>
2019-09-28 18:44:51 -0300

    Add files via upload

--- reagentes.sql
+++ reagentes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Set-2019 às 01:37
+-- Tempo de geração: 28-Set-2019 às 23:43
 -- Versão do servidor: 10.4.6-MariaDB
 -- versão do PHP: 7.1.31
 
@@ -29,12 +29,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `reagentes` (
+  `ID` int(11) NOT NULL,
   `idreagente` varchar(11) NOT NULL,
   `nome` varchar(60) CHARACTER SET utf8 NOT NULL,
   `marca` varchar(30) CHARACTER SET utf8 NOT NULL,
   `dtentrada` varchar(10) CHARACTER SET utf8 NOT NULL,
   `dtvalidade` varchar(10) NOT NULL,
   `quantidade` int(11) NOT NULL,
+  `qtdeminima` int(11) NOT NULL,
   `medida` varchar(2) NOT NULL,
   `inerte` varchar(5) NOT NULL,
   `dtsaida` varchar(10) NOT NULL
@@ -44,9 +46,10 @@ CREATE TABLE `reagentes` (
 -- Extraindo dados da tabela `reagentes`
 --
 
-INSERT INTO `reagentes` (`idreagente`, `nome`, `marca`, `dtentrada`, `dtvalidade`, `quantidade`, `medida`, `inerte`, `dtsaida`) VALUES
-('OI', 'OI', 'OI', '26/09/2019', '26/09/2019', 1, 'M', 'False', '  /  /'),
-('OI2', 'OI', 'OI', '26/09/2019', '26/09/2019', 1, 'M', 'False', '  /  /');
+INSERT INTO `reagentes` (`ID`, `idreagente`, `nome`, `marca`, `dtentrada`, `dtvalidade`, `quantidade`, `qtdeminima`, `medida`, `inerte`, `dtsaida`) VALUES
+(12, 'E12903', 'ACIDO SULFURICO2', 'TOP', '28/09/2019', '31/10/2019', 10, 5, '1', 'False', '  /  /'),
+(14, 'E12940', 'ACIDO SULFURICO4', 'TOP', '28/09/2019', '31/10/2019', 10, 5, '3', 'False', '  /  /'),
+(17, 'E12903', 'ACIDO SULFURICO2', 'TOP', '28/09/2019', '23/10/2019', 10, 10, '4', 'False', '  /  /');
 
 --
 -- Índices para tabelas despejadas
@@ -56,7 +59,17 @@ INSERT INTO `reagentes` (`idreagente`, `nome`, `marca`, `dtentrada`, `dtvalidade
 -- Índices para tabela `reagentes`
 --
 ALTER TABLE `reagentes`
-  ADD PRIMARY KEY (`idreagente`);
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `reagentes`
+--
+ALTER TABLE `reagentes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
